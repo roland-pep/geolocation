066339992564f7a9b3c9c095ae420c5baf226a47
Roland Kiraly <rolandkiraly94@gmail.com>
2024-03-12 11:10:28 +0000

    Remove unused imports and add console.log statement

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,8 @@
-import type { AppProps } from "next/app";
-import type { LayoutProps } from "@vercel/examples-ui/layout";
-import { getLayout } from "@vercel/examples-ui";
 import "@vercel/examples-ui/globals.css";
+import type { AppProps } from "next/app";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  const Layout = getLayout<LayoutProps>(Component);
-
+console.log('pageProps', pageProps)
   return (
     <div title="Geolocation">
       <Component {...pageProps} />
